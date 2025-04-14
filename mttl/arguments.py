60b5073743512120a148e61f68c7dda88c1253be
Lucas Caccia <lucas.page-caccia@mail.mcgill.ca>
2025-04-14 07:56:31 -0700

    was missing arg

--- mttl/arguments.py
+++ mttl/arguments.py
@@ -605,6 +605,7 @@ class EvaluationConfig(MultiExpertConfig, TransformArgs):
     es_metric: str = "loss"
     n_ng_iterations: int = 30  # number of iterations for LoraHub
     recompute_prototypes: bool = False
+    add_eos_to_downstream_targets: bool = True
 
 
 @dataclass
